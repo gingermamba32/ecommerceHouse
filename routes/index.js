a202bebf30e52e443839567a997599c6bc219b74
Michael_Montero <mmontero2012@gmail.com>
2016-12-11 16:48:22 -0700

    fix to the index path

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,15 @@ var path = require('path');
 // heroku statuc file server
 process.env.PWD = process.cwd()
 
+console.log(process.cwd());
+
 /* GET home page. */
 var html_dir = '../views/';
 router.get('/', function(req, res, next) {
-	res.sendFile(path.join(process.env.PWD+'/index.html'));
+	res.sendFile(path.join(process.env.PWD+'/views/index.html'));
   //res.sendFile('/Users/michaelmontero/Desktop/StoreFrontTemplate/views/index.html');
 });
+
+
 
 module.exports = router;
